752dd8e9b1b9ea8e7206265f1b3e9f6091387fb7
vdelarue <vdelarue@umich.edu>
2024-03-26 12:49:44 -0400

    modified formatting of cells and added icons

--- js/counterparty.js
+++ js/counterparty.js
@@ -1,4 +1,4 @@
-var filePath = "./data/counterparty-table-k.csv";
+var filePath = "./data/counterparty-table-fatal.csv";
 
 // populate the table
 updateTable();
@@ -6,7 +6,9 @@ updateTable();
 // dimensions and margins for the table
 var width = 1000,
     height = 800,
-    margin = { top: 20, right: 50, bottom: 100, left: 20 };
+    margin = { top: 50, right: 30, bottom: 30, left: 30 };
+const cellSize = 30;
+const iconSize = 25;
 
 // update the table with traffic accident data
 // this is a carry-over from the old table file -- I kept it here for when we add additional files that will require the file to be updated
@@ -21,7 +23,7 @@ function updateTable() {
 
 function createAccidentsTable(data) {
     var tableContainer = d3.select("#table");
-    const cellSize = 50; // fixed cell size for simplicity
+    const circleSize = 40;
 
     // find the maximum value in the dataset
     const maxValue = d3.max(data, row => {
@@ -32,11 +34,11 @@ function createAccidentsTable(data) {
 
     // max value is at least 1 to avoid log scale issues
     const adjustedMaxValue = maxValue > 0 ? maxValue : 1;
-    const maxCircleDiameter = cellSize * 0.9; // 90% of cellSize
+    const maxCircleDiameter = circleSize;
 
     var table = tableContainer.append("table")
-        .style("width", width + "px")
-        .style("height", height + "px")
+        // .style("width", width + "px")
+        // .style("height", height + "px")
         .style("margin-left", margin.left + "px")
         .style("margin-right", margin.right + "px")
         .attr("class", "table");
@@ -50,9 +52,26 @@ function createAccidentsTable(data) {
         .data(Object.keys(data[0]))
         .enter()
         .append("th")
-        .text(d => d)
-        .style("text-align", "center")
-        .style("font-weight", "bold");
+        .each(function (d, i) {
+            var th = d3.select(this);
+            if (d !== "Total" && d !== "2+ counterparties" && d !== "One-sided") {
+                var iconPath = "./icons/" + d.toLowerCase() + ".svg"; // Construct the path to the SVG
+                th.append("img")
+                    .attr("src", iconPath)
+                    .attr("alt", d) // accessibility: add an alt attribute
+                    .style("width", "auto") // set width of icons
+                    .style("height", iconSize + "px"); // set height to scale with the width
+            } else {
+                if (d == "2+ counterparties") {
+                    th.text("2+");
+                } else if (d == "One-sided") {
+                    th.text("1");
+                }
+                else {
+                    th.text(d);
+                }
+            }
+        });
 
     // table body rows
     var rows = tbody.selectAll("tr")
@@ -80,17 +99,14 @@ function createAccidentsTable(data) {
                 var cell = d3.select(this).style("position", "relative");
 
                 var diameter = calculateDiameter(+d.value, adjustedMaxValue, maxCircleDiameter);
-                const opacityScale = d3.scaleLinear()
-                    .domain([0, adjustedMaxValue])
-                    .range([0.5, 1]);
-                const opacity = opacityScale(+d.value);
+                const opacityScale = d3.scaleLog()
+                    .domain([1, adjustedMaxValue]) // domain starts at 1 to avoid log(0)
+                    .range([0.2, 1]);
+                const opacity = d.value == 0 ? 0 : opacityScale(+d.value);
 
                 var svg = cell.append("svg")
                     .attr("width", cellSize)
                     .attr("height", cellSize)
-                    .style("position", "absolute")
-                    .style("top", 0)
-                    .style("left", 0)
                     .style("overflow", "visible") // allow overflow for circle growing in size
                     .style("pointer-events", "none"); // make SVG transparent to mouse events
 
@@ -99,40 +115,53 @@ function createAccidentsTable(data) {
                     .style("pointer-events", "all"); // activate mouse events for the group
 
                 var circle = group.append("circle")
-                    .attr("cx", cellSize / 2)
-                    .attr("cy", cellSize / 2)
+                    .attr("cx", circleSize / 2)
+                    .attr("cy", circleSize / 2)
                     .attr("r", diameter / 2)
                     .style("fill", "red")
                     .style("opacity", opacity);
 
                 var text = group.append("text")
-                    .attr("x", cellSize / 2)
-                    .attr("y", cellSize / 2)
+                    .attr("x", circleSize / 2)
+                    .attr("y", circleSize / 2)
                     .attr("dy", ".35em")
                     .attr("text-anchor", "middle")
                     .text(d.value)
                     .style("font-size", "12px");
 
                 // apply hover effects to the group
-                group.on("mouseover", function () {
+                group.on("mouseover", function (event, d) {
                     circle.transition().duration(750)
-                        .attr("r", diameter) // double radius on hover
-                        .style("opacity", Math.max(0.5, opacity - 0.2));
+                        .attr("r", (diameter / 2) * 1.5) // increase radius on hover
+                        .style("opacity", opacity)
+                        .style("stroke", "black")
+                        .style("stroke-width", "1px");
 
                     text.transition().duration(750)
-                        .style("font-size", "24px"); // double the font size on hover
-                });
+                        .style("font-size", "18px"); // increase font size on hover
 
-                group.on("mouseout", function () {
-                    circle.transition().duration(750)
-                        .attr("r", diameter / 2) // reset the radius
-                        .style("opacity", opacity);
+                })
+                    .on("mouseout", function () {
+                        circle.transition().duration(750)
+                            .attr("r", diameter / 2) // reset the radius
+                            .style("opacity", opacity)
+                            .style("stroke", "none"); // remove the stroke
 
-                    text.transition().duration(750)
-                        .style("font-size", "12px"); // reset the font size
-                });
-            } else if (i === 0) { // add labels for the first column
-                cell.text(d.value);
+                        text.transition().duration(750)
+                            .style("font-size", "12px"); // reset the font size
+                    });
+
+            } else if (i === 0) {
+                if (d.value.toLowerCase() !== "total") {  // add icons for the first column
+                    var iconPath = "./icons/" + d.value.toLowerCase() + ".svg";
+                    cell.append("img")
+                        .attr("src", iconPath)
+                        .attr("alt", d.value) // accessibility: add an alt attribute
+                        .style("width", "auto") // set width of icons
+                        .style("height", iconSize + "px"); // set height to scale with the width
+                } else {
+                    cell.text(d.value);
+                }
             }
         });
 
